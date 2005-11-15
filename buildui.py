df193aed17d17542cb90ee554440315c32bdb7fb
Ehrayn <ehrayn@users.sourceforge.net>
2005-11-15 09:27:00 +0000

    MouseLabel is now handled right inside of ScWindow, with that   window looking up the widget to search.

--- buildui.py
+++ buildui.py
@@ -1,29 +1,34 @@
 import os
-import re
+import sys
 
-files = [ ['ScWindow.ui', 'B_ScWindow.py'],
-    ['Craft.ui', 'B_CraftWindow.py' ],
-    ['DisplayWindow.ui', 'B_DisplayWindow.py'],
-    ['ReportWindow.ui', 'B_ReportWindow.py'],
-    ['ItemLevel.ui', 'B_ItemLevel.py'],
-    ['CraftBar.ui', 'B_CraftBar.py'],
-    ['Options.ui', 'B_Options.py'],
-]
+files = ( ('ScWindow.ui', 'B_ScWindow.py'),
+    ('Craft.ui', 'B_CraftWindow.py'),
+    ('DisplayWindow.ui', 'B_DisplayWindow.py'),
+    ('ReportWindow.ui', 'B_ReportWindow.py'),
+    ('ItemLevel.ui', 'B_ItemLevel.py'),
+    ('CraftBar.ui', 'B_CraftBar.py'),
+    ('Options.ui', 'B_Options.py'),
+)
 
 for ui, py in files:
+    try:
+        if os.stat(ui).st_mtime < os.stat(py).st_mtime:
+            continue
+    except:
+        # We are happy to continue if a file is not found,
+        # as we will error on a missing input file, and go
+        # quietly on if the output file was missing
+        pass
     os.system('pyuic -o %s %s' % (py, ui))
-    if ui == 'ScWindow.ui':
+
+    # Make this Win32 text
+    if os.name == 'nt':
         f = open(py, 'r')
         scstr = f.read()
         f.close()
-        scstr = re.sub('from qt import \*', 'from qt import *\n' \
-                     + 'from MultiTabBar import *\n' \
-                     + 'from SearchingCombo import *', scstr)
-        scstr = re.sub('QDialog', 'QMainWindow', scstr)
-        scstr = re.sub('QComboBox', 'SearchingCombo', scstr)
-        scstr = re.sub('modal[^,]*,', '', scstr)
         f = open(py, 'w')
         f.write(scstr)
         f.close()
+        del scstr
 
-            
+    sys.stdout.write("Generated %s from %s\n" % (py, ui))
