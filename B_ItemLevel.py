a928234b555fc05f8f061704bd478a388f72a9f6
Ehrayn <ehrayn@users.sourceforge.net>
2005-11-25 09:01:09 +0000

    Restructure ItemLevel dialog to allow the user to modify the   level, af/dps or shield material and get instant feedback about   the calculation.

--- B_ItemLevel.py
+++ B_ItemLevel.py
@@ -2,13 +2,15 @@
 
 # Form implementation generated from reading ui file 'ItemLevel.ui'
 #
-# Created: Wed Sep 28 18:57:25 2005
+# Created: Fri Nov 25 02:51:32 2005
 #      by: The PyQt User Interface Compiler (pyuic) 3.15
 #
 # WARNING! All changes made in this file will be lost!
 
 
 from qt import *
+#Import the Custom Widgets
+from SearchingCombo import *
 
 
 class B_ItemLevel(QDialog):
@@ -21,52 +23,63 @@ class B_ItemLevel(QDialog):
 
 
         self.ButtonGroup3 = QButtonGroup(self,"ButtonGroup3")
-        self.ButtonGroup3.setGeometry(QRect(20,9,117,111))
+        self.ButtonGroup3.setGeometry(QRect(6,6,117,111))
         self.ButtonGroup3.setLineWidth(0)
 
         self.Armor = QRadioButton(self.ButtonGroup3,"Armor")
-        self.Armor.setGeometry(QRect(3,2,85,20))
+        self.Armor.setGeometry(QRect(5,2,85,20))
         self.Armor.setChecked(1)
 
         self.ClothArmor = QRadioButton(self.ButtonGroup3,"ClothArmor")
-        self.ClothArmor.setGeometry(QRect(3,23,85,20))
+        self.ClothArmor.setGeometry(QRect(5,23,85,20))
 
         self.Weapon = QRadioButton(self.ButtonGroup3,"Weapon")
         self.Weapon.setGeometry(QRect(3,45,85,20))
 
         self.Shield = QRadioButton(self.ButtonGroup3,"Shield")
-        self.Shield.setGeometry(QRect(3,66,85,20))
+        self.Shield.setGeometry(QRect(5,66,85,20))
 
         self.ReinforcedShield = QRadioButton(self.ButtonGroup3,"ReinforcedShield")
-        self.ReinforcedShield.setGeometry(QRect(3,87,108,20))
+        self.ReinforcedShield.setGeometry(QRect(5,87,108,20))
 
-        self.TextLabel1 = QLabel(self,"TextLabel1")
-        self.TextLabel1.setGeometry(QRect(151,14,53,13))
+        self.LevelLabel = QLabel(self,"LevelLabel")
+        self.LevelLabel.setGeometry(QRect(130,10,45,16))
 
-        self.OK = QPushButton(self,"OK")
-        self.OK.setGeometry(QRect(30,124,81,26))
+        self.Level = QLineEdit(self,"Level")
+        self.Level.setGeometry(QRect(185,7,60,22))
 
-        self.Cancel = QPushButton(self,"Cancel")
-        self.Cancel.setGeometry(QRect(130,124,79,26))
-
-        self.ShieldType = QComboBox(0,self,"ShieldType")
-        self.ShieldType.setGeometry(QRect(105,61,113,22))
+        self.AFDPSLabel = QLabel(self,"AFDPSLabel")
+        self.AFDPSLabel.setGeometry(QRect(130,53,45,16))
 
         self.AFDPS = QLineEdit(self,"AFDPS")
-        self.AFDPS.setGeometry(QRect(140,31,74,22))
+        self.AFDPS.setGeometry(QRect(185,50,60,22))
+
+        self.ShieldType = SearchingCombo(self,"ShieldType")
+        self.ShieldType.setGeometry(QRect(135,93,110,22))
+
+        self.OK = QPushButton(self,"OK")
+        self.OK.setGeometry(QRect(11,124,58,26))
+
+        self.Cancel = QPushButton(self,"Cancel")
+        self.Cancel.setGeometry(QRect(187,124,58,26))
 
         self.languageChange()
 
-        self.resize(QSize(235,158).expandedTo(self.minimumSizeHint()))
+        self.resize(QSize(250,158).expandedTo(self.minimumSizeHint()))
         self.clearWState(Qt.WState_Polished)
 
+        self.connect(self.Armor,SIGNAL("clicked()"),self.TypeChanged)
         self.connect(self.ClothArmor,SIGNAL("clicked()"),self.TypeChanged)
         self.connect(self.Weapon,SIGNAL("clicked()"),self.TypeChanged)
         self.connect(self.Shield,SIGNAL("clicked()"),self.TypeChanged)
         self.connect(self.ReinforcedShield,SIGNAL("clicked()"),self.TypeChanged)
-        self.connect(self.OK,SIGNAL("clicked()"),self.OK_pressed)
-        self.connect(self.Cancel,SIGNAL("clicked()"),self.Cancel_pressed)
-        self.connect(self.AFDPS,SIGNAL("textChanged(const QString&)"),self.FixInput)
+        self.connect(self.Level,SIGNAL("textChanged(const QString&)"),self.LevelChanged)
+        self.connect(self.Level,SIGNAL("lostFocus()"),self.LevelDone)
+        self.connect(self.AFDPS,SIGNAL("textChanged(const QString&)"),self.AFDPSChanged)
+        self.connect(self.AFDPS,SIGNAL("lostFocus()"),self.AFDPSDone)
+        self.connect(self.ShieldType,SIGNAL("activated(const QString&)"),self.ShieldChanged)
+        self.connect(self.OK,SIGNAL("clicked()"),self.OkClicked)
+        self.connect(self.Cancel,SIGNAL("clicked()"),self.CancelClicked)
 
 
     def languageChange(self):
@@ -77,22 +90,35 @@ class B_ItemLevel(QDialog):
         self.Weapon.setText(self.__tr("Weapon"))
         self.Shield.setText(self.__tr("Shield"))
         self.ReinforcedShield.setText(self.__tr("Reinforced Sheild"))
-        self.TextLabel1.setText(self.__tr("AF or DPS"))
+        self.LevelLabel.setText(self.__tr("Level:"))
+        self.AFDPSLabel.setText(self.__tr("AF/DPS:"))
         self.OK.setText(self.__tr("OK"))
         self.Cancel.setText(self.__tr("Cancel"))
 
 
-    def Cancel_pressed(self):
-        print "B_ItemLevel.Cancel_pressed(): Not implemented yet"
-
-    def OK_pressed(self):
-        print "B_ItemLevel.OK_pressed(): Not implemented yet"
-
     def TypeChanged(self):
         print "B_ItemLevel.TypeChanged(): Not implemented yet"
 
-    def FixInput(self,a0):
-        print "B_ItemLevel.FixInput(const QString&): Not implemented yet"
+    def LevelChanged(self,a0):
+        print "B_ItemLevel.LevelChanged(const QString&): Not implemented yet"
+
+    def LevelDone(self):
+        print "B_ItemLevel.LevelDone(): Not implemented yet"
+
+    def AFDPSChanged(self,a0):
+        print "B_ItemLevel.AFDPSChanged(const QString&): Not implemented yet"
+
+    def AFDPSDone(self):
+        print "B_ItemLevel.AFDPSDone(): Not implemented yet"
+
+    def ShieldChanged(self,a0):
+        print "B_ItemLevel.ShieldChanged(const QString&): Not implemented yet"
+
+    def OkClicked(self):
+        print "B_ItemLevel.OkClicked(): Not implemented yet"
+
+    def CancelClicked(self):
+        print "B_ItemLevel.CancelClicked(): Not implemented yet"
 
     def __tr(self,s,c = None):
         return qApp.translate("B_ItemLevel",s,c)
--- ItemLevel.py
+++ ItemLevel.py
@@ -6,53 +6,137 @@ import re
 class ItemLevel(B_ItemLevel):
     def __init__(self,parent = None,name = None,modal = 0,fl = 0):
         B_ItemLevel.__init__(self,parent,name,modal,fl)
-        self.ShieldType.insertStrList(list(ShieldTypes))
-        self.AFDPS.setText('102')
+        self.ShieldType.hide()
         self.level = 51
-        self.ShieldType.setEnabled(0)
+        self.afdps = 102
+        self.inchange = True
+        self.Level.setText(str(self.level))
+        self.inchange = False
+        self.TypeChanged()
 
-    def OK_pressed(self):
-        self.computeLevel()
+    def OkClicked(self):
         self.done(self.level)
 
-    def computeLevel(self):
-        if self.Armor.isChecked():
-            self.level = int(str(self.AFDPS.text())) / 2
-        elif self.ClothArmor.isChecked():
-            self.level = int(str(self.AFDPS.text()))
-        elif self.Weapon.isChecked():
-            self.level = int((float(str(self.AFDPS.text())) - 1.2) / 0.3)
-        elif self.Shield.isChecked():
-            if self.ShieldType.currentItem() == 0:
-                self.level = 2
-            else:
-                self.level = self.ShieldType.currentItem() * 5
-        elif self.ReinforcedShield.isChecked():
-            self.level = (self.ShieldType.currentItem() + 1) * 5 + 1
-            
-    def TypeChanged(self):
-        if self.Armor.isChecked():
-            self.AFDPS.setText('102')
-            self.AFDPS.setEnabled(1)
-            self.ShieldType.setEnabled(0)
-        elif self.ClothArmor.isChecked():
-            self.AFDPS.setText('51')
-            self.AFDPS.setEnabled(1)
-            self.ShieldType.setEnabled(0)
-        elif self.Weapon.isChecked():
-            self.AFDPS.setText('16.5')
-            self.AFDPS.setEnabled(1)
-            self.ShieldType.setEnabled(0)
-        elif self.Shield.isChecked():
-            self.AFDPS.setEnabled(0)
-            self.ShieldType.setEnabled(1)
-        elif self.ReinforcedShield.isChecked():
-            self.AFDPS.setEnabled(0)
-            self.ShieldType.setEnabled(1)
-        
-    def Cancel_pressed(self):
+    def CancelClicked(self):
         self.done(-1)
 
-    def FixInput(self,a0):
-        esc = re.sub('[^\d.]', '', str(self.AFDPS.text()))
-        self.AFDPS.setText(esc)
+    def ShieldChanged(self, value):
+        if self.inchange: return
+        self.inchange = True
+
+        if self.Shield.isChecked():
+            self.level = max(self.ShieldType.currentItem() * 5, 2)
+        elif self.ReinforcedShield.isChecked():
+            self.level = self.ShieldType.currentItem() * 5 + 26
+        self.Level.setText(str(self.level))
+
+        self.afdps = float(self.level) * 0.3 + 1.2
+        self.AFDPS.setText(str(self.afdps))
+
+        self.inchange = False
+
+    def AFDPSChanged(self, value):
+        if self.inchange: return
+        self.inchange = True
+
+        if isinstance(value, bool):
+            fix = value
+            value = self.AFDPS.text()
+        else:
+            fix = False
+
+        try:
+            if self.Armor.isChecked():
+                self.afdps = int(int(str(value)) / 2) * 2
+                self.level = int(self.afdps / 2)
+            elif self.ClothArmor.isChecked():
+                self.afdps = int(int(str(value)) / 2) * 2
+                self.level = self.afdps
+            else:
+                self.afdps = float(str(value))
+                self.level = max(min(int((self.afdps - 1.2) / 0.3), 51), 1)
+                self.afdps = float(self.level) * 0.3 + 1.2
+                if self.Shield.isChecked():
+                    tier = min(int(self.level / 5), 9)
+                    self.level = max(tier * 5, 2)
+                    self.ShieldType.setCurrentItem(tier)
+                elif self.ReinforcedShield.isChecked():
+                    tier = max(int((self.level - 26) / 5), 0)
+                    self.level = tier * 5 + 26
+                    self.ShieldType.setCurrentItem(tier)
+        except:
+            self.level = 1
+            self.ShieldType.setCurrentItem(0)
+
+        self.Level.setText(str(self.level))
+
+        if fix: 
+            self.AFDPS.setText(str(self.afdps))
+
+        self.inchange = False
+            
+    def AFDPSDone(self):
+        self.AFDPSChanged(True)
+
+    def LevelChanged(self, value):
+        if self.inchange: return
+        self.inchange = True
+
+        if isinstance(value, bool):
+            fix = value
+            value = self.Level.text()
+        else:
+            fix = False
+
+        try:
+            self.level = max(min(int(str(value)), 51), 1)
+        except:
+            self.level = 1
+
+        if self.Shield.isChecked():
+            tier = min(int(self.level / 5), 9)
+            self.level = max(tier * 5, 2)
+            self.ShieldType.setCurrentItem(tier)
+        elif self.ReinforcedShield.isChecked():
+            tier = max(int((self.level - 26) / 5), 0)
+            self.level = tier * 5 + 26
+            self.ShieldType.setCurrentItem(tier)
+
+        if self.Armor.isChecked():
+            self.afdps = self.level * 2
+        elif self.ClothArmor.isChecked():
+            self.afdps = self.level
+        else:
+            self.afdps = float(self.level) * 0.3 + 1.2
+        self.AFDPS.setText(str(self.afdps))
+
+        if fix:
+            self.Level.setText(str(self.level))
+
+        self.inchange = False
+        
+    def LevelDone(self):
+        self.LevelChanged(True)
+
+    def TypeChanged(self):
+        self.inchange = False
+
+        if self.Armor.isChecked() or self.ClothArmor.isChecked():
+            self.AFDPSLabel.setText('AF')
+        else:
+            self.AFDPSLabel.setText('DPS')
+
+        if self.Armor.isChecked() or self.ClothArmor.isChecked() \
+                                  or self.Weapon.isChecked():
+            self.ShieldType.hide()
+            self.LevelChanged(True)
+            return
+
+        if self.Shield.isChecked():
+            self.ShieldType.clear()
+            self.ShieldType.insertStrList(list(ShieldTypes))
+        elif self.ReinforcedShield.isChecked():
+            self.ShieldType.clear()
+            self.ShieldType.insertStrList(list(ShieldTypes[4:]))
+        self.LevelChanged(True)
+        self.ShieldType.show()
