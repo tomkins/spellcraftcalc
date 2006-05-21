00232c2587e48483578c08fcc26f977b6eb31c25
Ehrayn <ehrayn@users.sourceforge.net>
2006-05-21 21:49:33 +0000

    Correct SearchingCombo for sometimes-editable states.

--- SearchingCombo.py
+++ SearchingCombo.py
@@ -1,10 +1,11 @@
 from qt import *
 import sys
 import string
+
 class SearchingCombo(QComboBox):
-    def __init__(self, parent=None, name=None):
-        QComboBox.__init__(self, 0, parent, name)
-        
+    def __init__(self, parent=None, name=None, editable=False):
+        QComboBox.__init__(self, editable, parent, name)
+
     def buildItemKeys(self):
         keys = []
         for i in range(0, self.count()):
@@ -19,24 +20,27 @@ class SearchingCombo(QComboBox):
             if ci > 0: ci -= 1
             self.setCurrentItem(ci)
             self.emit(SIGNAL("activated(const QString&)"),(self.currentText(),))
+            return
         elif keycode == Qt.Key_Down:
             ci = self.currentItem()
             if ci != (self.count() - 1): ci += 1
             self.setCurrentItem(ci)
             self.emit(SIGNAL("activated(const QString&)"),(self.currentText(),))
-        else:
+            return
+        elif not self.editable():
             key = str(e.text()).upper()
             itemkeys = self.buildItemKeys()
             indexlist = []
             for i in range(0, len(itemkeys)):
                 if key in itemkeys[i]:
                     indexlist.append(i)
-            if len(indexlist) == 0:
-                QComboBox.keyPressEvent(self, e)
+            if len(indexlist):
+                if self.currentItem() >= indexlist[-1]:
+                    self.setCurrentItem(indexlist[0])
+                else:
+                    i = filter(lambda x: x > self.currentItem(), indexlist)[0]
+                    self.setCurrentItem(i)
+                self.emit(SIGNAL("activated(const QString&)"),(self.currentText(),))
                 return
-            if self.currentItem() >= indexlist[-1]:
-                self.setCurrentItem(indexlist[0])
-            else:
-                i = filter(lambda x: x > self.currentItem(), indexlist)[0]
-                self.setCurrentItem(i)
-            self.emit(SIGNAL("activated(const QString&)"),(self.currentText(),))
+        QComboBox.keyPressEvent(self, e)
+        return
