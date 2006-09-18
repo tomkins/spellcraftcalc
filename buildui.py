e7d4057ee17e7f48d03fe663b2888b6fe4a8a781
Ehrayn <ehrayn@users.sourceforge.net>
2006-09-18 01:40:38 +0000

    A first-blush at the migration of the ui files to Qt4

--- buildui.py
+++ buildui.py
@@ -2,13 +2,13 @@ import os
 import sys
 
 files = (
-    ('ScWindow.ui', 'B_ScWindow.py'),
-    ('CraftWindow.ui', 'B_CraftWindow.py'),
-    ('DisplayWindow.ui', 'B_DisplayWindow.py'),
-    ('ReportWindow.ui', 'B_ReportWindow.py'),
-    ('ItemLevel.ui', 'B_ItemLevel.py'),
-    ('CraftBar.ui', 'B_CraftBar.py'),
-    ('Options.ui', 'B_Options.py'),
+    ('ScWindow.ui4', 'B_ScWindow.py'),
+    ('CraftWindow.ui4', 'B_CraftWindow.py'),
+    ('DisplayWindow.ui4', 'B_DisplayWindow.py'),
+    ('ReportWindow.ui4', 'B_ReportWindow.py'),
+    ('ItemLevel.ui4', 'B_ItemLevel.py'),
+    ('CraftBar.ui4', 'B_CraftBar.py'),
+    ('Options.ui4', 'B_Options.py'),
 )
 
 for ui, py in files:
@@ -20,8 +20,8 @@ for ui, py in files:
         # as we will error on a missing input file, and go
         # quietly on if the output file was missing
         pass
-    os.system('%s -o %s %s' % (
-              os.path.join(os.path.dirname(sys.executable),'pyuic'), py, ui))
+    os.system('%s -d -o %s %s' % (
+              os.path.join(os.path.dirname(sys.executable),'pyuic4'), py, ui))
 
     # Make this Win32 text
     if os.name == 'nt':
