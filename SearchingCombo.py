8a11a35454091c35cf19b7d4ab808ac907a752b0
James Lamanna <kort@users.sourceforge.net>
2006-09-18 03:47:43 +0000

    Update SearchingCombo for Qt4

--- SearchingCombo.py
+++ SearchingCombo.py
@@ -4,24 +4,26 @@
 #
 # See NOTICE.txt for copyrights and grant of license
 
-from qt import *
 import sys
 import string
 
+from PyQt4.QtGui import QComboBox
+
 class SearchingCombo(QComboBox):
     def __init__(self, parent=None, name=None, editable=False):
-        QComboBox.__init__(self, editable, parent, name)
+        QComboBox.__init__(self, parent)
+        self.setEditable(editable)
 
     def buildItemKeys(self):
         keys = []
         for i in range(0, self.count()):
-            txt = str(self.listBox().item(i).text())
-            keys.append(string.join(map(lambda s: s[0], str(txt).split()), "").upper())
+            txt = str(self.itemText(i))
+            keys.append(string.join(map(lambda s: s[0], txt.split()), "").upper())
         return keys
 
     def keyPressEvent(self, e):
         keycode = e.key()
-        if len(e.text()) > 0 and not self.editable():
+        if len(e.text()) > 0 and not self.isEditable():
             key = str(e.text()).upper()
             indexlist = []
             if len(key) == 1:
@@ -31,10 +33,10 @@ class SearchingCombo(QComboBox):
                         indexlist.append(i)
             if len(indexlist):
                 if self.currentItem() >= indexlist[-1]:
-                    self.setCurrentItem(indexlist[0])
+                    self.setCurrentIndex(indexlist[0])
                 else:
-                    i = filter(lambda x: x > self.currentItem(), indexlist)[0]
-                    self.setCurrentItem(i)
+                    i = filter(lambda x: x > self.currentIndex(), indexlist)[0]
+                    self.setCurrentIndex(i)
                 self.emit(SIGNAL("activated(const QString&)"),(self.currentText(),))
             e.accept()
             return
