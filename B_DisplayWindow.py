5b6c0d18141cc8286818e9151d54c8faa9c0accc
Ehrayn <ehrayn@users.sourceforge.net>
2006-10-06 05:15:50 +0000

    No Qt3 :)  No Wire Hangers either!

--- B_DisplayWindow.py
+++ B_DisplayWindow.py
@@ -8,7 +8,7 @@
 # WARNING! All changes made in this file will be lost!
 
 import sys
-from PyQt4 import QtCore, QtGui, Qt3Support
+from PyQt4 import QtCore, QtGui
 
 class Ui_B_DisplayWindow(object):
     def setupUi(self, B_DisplayWindow):
