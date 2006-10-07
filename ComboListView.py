0425fa71474f98e5dd6af6c1478918a71a0bd148
James Lamanna <kort@users.sourceforge.net>
2006-10-07 22:28:25 +0000

    test for width fix on view

--- ComboListView.py
+++ ComboListView.py
@@ -5,6 +5,30 @@ class ComboListView(QListView):
     def __init__(self, p, cb):
         QListView.__init__(self, p)
         self.combobox = cb
+        self.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
+
+    def showEvent(self, e):
+        e.accept()
+        model = self.model()
+        rows = model.rowCount()
+        fm = QFontMetrics(self.font())
+        mx = 0
+
+        for i in range(0, rows):
+            idx = model.index(i, 0)
+            data = model.data(idx)
+            s = str(data.toString())
+            print s
+            print fm.size(Qt.TextSingleLine, s).width()
+            mx = max(mx, fm.size(Qt.TextSingleLine, s).width())
+
+        mx += 25
+
+        self.setGeometry(self.pos().x(), self.pos().y(), mx, self.height())
+        self.parent().setGeometry(self.parent().pos().x(),
+            self.parent().pos().y(), mx, self.parent().height())
+            
+
 
     def keyPressEvent(self, e):
 
