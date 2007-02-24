868d641d168ba57ccf7498480a4622af7a2f383e
Ehrayn <ehrayn@users.sourceforge.net>
2007-02-24 23:09:06 +0000

    Ok, this looks sucky on WinXP Display Style, but we can't distinguish xp style from classic style.  GRRRRR

--- GroupFrame.py
+++ GroupFrame.py
@@ -1,4 +1,5 @@
-from PyQt4.QtGui import QFrame, QPainter, QStyle, QStyleOptionFrameV2
+from PyQt4.QtGui import QFrame, QPainter, QStyle
+from PyQt4.QtGui import QStyleOptionFrame, QStyleOptionFrameV2
 
 class GroupFrame(QFrame):
     def __init__(self, parent = None):
@@ -6,8 +7,13 @@ class GroupFrame(QFrame):
 
     def paintEvent(self, pevent):
         painter = QPainter(self)
-        groupframe = QStyleOptionFrameV2()
-        groupframe.init(self);
-        self.style().drawPrimitive(QStyle.PE_FrameGroupBox,
-                                   groupframe, painter, self)
+        styleoptions = QStyleOptionFrameV2()
+        styleoptions.init(self);
+        #styleoptions.FrameFeatures = QStyleOptionFrameV2.None
+        if str(self.style().objectName()).lower() == "windowsxp":
+          # it sucks, but windowsxp in classic view displays no groupframe
+          frame = QStyle.PE_FrameTabWidget
+        else:
+          frame = QStyle.PE_FrameGroupBox
+        self.style().drawPrimitive(frame, styleoptions, painter, self)
 
