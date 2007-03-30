3be09616fd1679ceba47d233e64d9076c77886f4
Ehrayn <ehrayn@users.sourceforge.net>
2007-03-30 06:46:00 +0000

    Show us WHERE

--- Ethinarg.py
+++ Ethinarg.py
@@ -612,7 +612,7 @@ class EthinargTestWindow(QDialog, Ui_B_Ethinarg):
         self.scwin.addItem(item)
 
         QMessageBox.information(self, "Item Added!",
-            "'%s' Successfully Added to Template!" % item.ItemName)
+            "'%s' added to the '%s' slot" % (item.ItemName, item.Location,))
 
     def event(self, e):
         if e.type() == LoginFailedEvent:
