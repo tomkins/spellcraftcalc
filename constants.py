b84c525a0ed5a82c68eb55914f9d648c07125eb8
Ehrayn <ehrayn@users.sourceforge.net>
2007-04-30 08:18:19 +0000

    Bump to 2.01 dev

--- constants.py
+++ constants.py
@@ -21,7 +21,7 @@ __all__ = [
   'ArmorTabList', 'WeaponTabList', 'FocusTabList',
 ]
 
-ScVersion = "Kort's Spellcrafting Calulator 2.00"
+ScVersion = "Kort's Spellcrafting Calulator 2.01"
 
 from Character import *
 from tuple2 import * 
