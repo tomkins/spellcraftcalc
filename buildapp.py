6606ad403505275e8cda3e6a619e5f632ce89943
James Lamanna <kort@users.sourceforge.net>
2007-07-07 03:33:40 +0000

    add all dependencies

--- buildapp.py
+++ buildapp.py
@@ -7,8 +7,31 @@
 #% python setup.py py2app --help
 #"""
 
+import glob
 from distutils.core import setup
 import py2app
+import Ft.Lib.DistExt.Py2Exe
+py2app_options = dict(
+    iconfile='kscraft.icns',
+    includes=['sip', 'cmd', 'Ft.Xml.XPath.ParsedPredicateList',
+        'Ft.Xml.XPath.ParsedAbbreviatedAbsoluteLocationPath',
+        'Ft.Xml.XPath.ParsedAbbreviatedRelativeLocationPath',
+        'Ft.Xml.XInclude',
+        'Ft.Xml.Xslt.XPatterns',
+        ],
+)
 setup(
     app = ['Spellcraft.pyw'],
+    data_files=[
+        ('.', ['./LICENSE.txt']),
+        ('.', ['./NOTICE.txt']),
+        ('.', ['./CHANGES.txt']),
+        ('.', ['./SC.rcc']),
+        ('reports', ['reports/Reports.txt']), 
+        ('reports', glob.glob("reports/*.xml")),
+        ('reports', glob.glob("reports/*.xsl")),
+    ],
+    options=dict(
+        py2app=py2app_options,
+    )
 )
